877f4cd54e8bcb23fd4eed589e2b675fd551ddd9
NITRO 5 <Peerapatpee@hotmail.com>
2020-07-31 02:13:58 +0700

    แก้ไขหน้า about me

--- app/src/main/java/com/example/android/navigation/ContactFragment.kt
+++ app/src/main/java/com/example/android/navigation/ContactFragment.kt
@@ -22,7 +22,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class RulesFragment : Fragment() {
+class ContactFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
